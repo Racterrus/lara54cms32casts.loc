518d38073fece808cc1011743d00e72a81002966
Racterrus <antonioracter@yandex.ru>
2017-07-11 18:29:18 +0300

    сделали вывод только опубликованных статей

--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Seeder;
 use Faker\Factory;
+use Carbon\Carbon;
 
 class PostsTableSeeder extends Seeder
 {
@@ -18,11 +19,15 @@ class PostsTableSeeder extends Seeder
 	    //generate 10 posts
 	    $posts = [];
 	    $faker = Factory::create();
+	    $date = Carbon::create(2017, 7, 7, 9);
 
 	    for($i = 1; $i <= 10; $i++){
 
 		    $image = "Post_Image_" . rand(1, 5) . ".jpg";
-		    $date = date("Y-m-d H:i:s", strtotime("2017-07-05 07:00:00 +{$i} days"));
+		    //$date = date("Y-m-d H:i:s", strtotime("2017-07-05 07:00:00 +{$i} days"));
+		    $date->addDays(1);
+		    $publishedDate = clone($date);
+		    $createdDate = clone($date);
 
 	    	$posts[] = [
 
@@ -32,8 +37,9 @@ class PostsTableSeeder extends Seeder
 			    'body' => $faker->paragraphs(rand(10, 15), true),
 			    'slug' => $faker->slug(),
 			    'image' => rand(0, 1) == 1 ? $image : NULL,
-			    'created_at' => $date,
-			    'updated_at' => $date,
+			    'created_at' => $createdDate,
+			    'updated_at' => $createdDate,
+			    'published_at' => $i < 5 ? $publishedDate : ( rand(0, 1) == 0 ? NULL : $publishedDate->addDays(4))
 		    ];
 
 	    }
